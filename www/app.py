2490e18148d657f071688e3d67baf80936fae729
MiniBee <hongyue_pei@163.com>
2018-01-24 16:34:51 +0800

    modified ip

--- www/app.py
+++ www/app.py
@@ -7,13 +7,13 @@ from aiohttp import web
 
 
 def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>')
+    return web.Response(body=u'<h1>Awesome</h1>')
 
 @asyncio.coroutine
 def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '39.106.188.182', 9000)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
